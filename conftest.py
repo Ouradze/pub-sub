218f924576c8355877ec17a16048c452e5c6d84d
Ouradze <me.raddadi@gmail.com>
2021-07-27 17:54:26 +0200

    feat(messages): add dependencies for messages management and futur tests
    - Add desert to enable easy serialization and deserialization of
    messages
    - Add factory boy to create fake data
    - Add snapshottest to create snapshot of said date easily
    - Factory boy doesn't expose types, as such we ignore it in mypy
    - Add seed to the conf test to reproduce tests
    - Add py.typed to explain to mypy that yes we do expose types

--- conftest.py
+++ conftest.py
@@ -1,4 +1,16 @@
 import os
 
+import factory.random
+import pytest
+
 
 os.environ.setdefault("PYTHONBREAKPOINT", "ipdb.set_trace")
+
+
+@pytest.fixture(autouse=True)
+def seed_factory_boy() -> None:
+    """
+    Give the same seed to factory-boy to prevent snapshot tests from failing because
+    of changing data.
+    """
+    factory.random.reseed_random("16159953954273")
